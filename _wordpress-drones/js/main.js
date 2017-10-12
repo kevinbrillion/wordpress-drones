8466e47c2d660b7d520e86658d480ce08f9dd5a2
ChaseMeImFamous <kevin.brillion@gmail.com>
2017-10-12 17:06:34 +0200

    :wrench: test slack github

--- _wordpress-drones/js/main.js
+++ _wordpress-drones/js/main.js
@@ -1,4 +1,4 @@
-/* Wordpress Parrot Project – Author: Kevin Brillion <kevin.brillion@hetic.net> Version: 0.1.0+4 */
+/* Wordpress Parrot Project – Author: Kevin Brillion <kevin.brillion@hetic.net> Version: 0.1.0+6 */
 (function () {
 'use strict';
 
--- _wordpress-drones/lib/scripts-and-styles.php
+++ _wordpress-drones/lib/scripts-and-styles.php
@@ -5,7 +5,7 @@
  */
 function wordpress_drones_scripts() {
 
-    wp_enqueue_script( 'scripts', THEMEURI . '/js/main.js', [], '0.1.0+4-1507819939638', true );
+    wp_enqueue_script( 'scripts', THEMEURI . '/js/main.js', [], '0.1.0+6-1507820759863', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'wordpress_drones_scripts' );
@@ -15,7 +15,7 @@ add_action( 'wp_enqueue_scripts', 'wordpress_drones_scripts' );
  */
 function wordpress_drones_styles() {
 
-    wp_enqueue_style( 'styles', get_stylesheet_uri(), [], '0.1.0+4-1507819939637');
+    wp_enqueue_style( 'styles', get_stylesheet_uri(), [], '0.1.0+8-1507820759862');
 
 }
 add_action( 'wp_enqueue_scripts', 'wordpress_drones_styles' );
