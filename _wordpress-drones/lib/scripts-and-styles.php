b9560a0d644d0edc09275327f94ec559e3eeef4d
NicolasBernaux <bernaux.n@gmail.com>
2017-11-27 12:55:59 +0100

    :sparkles: Home

--- _wordpress-drones/lib/scripts-and-styles.php
+++ _wordpress-drones/lib/scripts-and-styles.php
@@ -5,7 +5,7 @@
  */
 function wordpress_drones_scripts() {
 
-    wp_enqueue_script( 'scripts', THEMEURI . '/js/main.js', [], '0.1.0+46-1511399036667', true );
+    wp_enqueue_script( 'scripts', THEMEURI . '/js/main.js', [], '0.1.0+48-1511783223946', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'wordpress_drones_scripts' );
@@ -15,7 +15,7 @@ add_action( 'wp_enqueue_scripts', 'wordpress_drones_scripts' );
  */
 function wordpress_drones_styles() {
 
-    wp_enqueue_style( 'styles', get_stylesheet_uri(), [], '0.1.0+183-1511399036666');
+    wp_enqueue_style( 'styles', get_stylesheet_uri(), [], '0.1.0+224-1511783223945');
 
 }
 add_action( 'wp_enqueue_scripts', 'wordpress_drones_styles' );
--- src/templates/home.php
+++ src/templates/home.php
@@ -4,9 +4,9 @@
         <img class="landing__img" src="wp-content/themes/wordpress-drones/_wordpress-drones/img/drone_landing.png" alt="">
         <h1 class="landing__title"><?php $title = get_the_title(); echo $title; ?></h1>
         <h2 class="landing__subtitle"><?php bloginfo( 'description' ); ?> </h2>
+        <a href="#" class="landing__discover btn__discover">Decouvrir</a>
     </div>
     <div class="discover">
-        <a href="#" class="discover__discover btn__discover">Decouvrir</a>
         <div class="discover__imgContainer">
             <img src="wp-content/themes/wordpress-drones/_wordpress-drones/img/discover.png" alt="" class="discover__img">
         </div>
@@ -24,30 +24,38 @@
         <a href="#" class="section__btn btn__discover">Decouvrir</a>
 
     </section>
-    <section class="secondSection homeSection">
-        <img src="wp-content/themes/wordpress-drones/_wordpress-drones/img/section2.png" alt="" class="secondSection__img">
-        <h2 class="section__title secondSection__title">02</h2>
-        <h3 class="section__subTitle">Pars en mission, rien ne peut t’arrêter !</h3>
-        <p>La relève du jeu est sous vos yeux !
-            Les minidrones se faufilent et s'invitent partout, de la cour de récré jusque dans l'open space.
-            Dotés des meilleures technologies, ils vous font enchaîner courses, acrobaties, jets de projectiles...
-            <br><br>
-            Aucune limite à votre imagination !
-        </p>
 
-        <a href="#" class="section__btn btn__discover">Decouvrir</a>
+    </section>
+    <section class="secondSection homeSection">
+        <img src="wp-content/themes/wordpress-drones/_wordpress-drones/img/section2.png" alt="" class="secondSection__img--small">
+        <div class="section__text">
+            <h2 class="section__title secondSection__title">02</h2>
+            <h3 class="section__subTitle">Pars en mission, rien ne peut t’arrêter !</h3>
+            <p>La relève du jeu est sous vos yeux !
+                Les minidrones se faufilent et s'invitent partout, de la cour de récré jusque dans l'open space.
+                Dotés des meilleures technologies, ils vous font enchaîner courses, acrobaties, jets de projectiles...
+                <br><br>
+                Aucune limite à votre imagination !
+            </p>
+            <a href="#" class="section__btn btn__discover">Decouvrir</a>
+
+        </div>
+
+        <img src="wp-content/themes/wordpress-drones/_wordpress-drones/img/section2.png" alt="" class="secondSection__img--big">
+
     </section>
     <section class="thridSection homeSection">
-        <img src="wp-content/themes/wordpress-drones/_wordpress-drones/img/section3.png" alt="" class="secondSection__img">
-        <h2 class="section__title thirdSection__title">03</h2>
-        <h3 class="section__subTitle">Des technologies pour les pros</h3>
-        <p>Réussir des vidéos de grande qualité avec un drone n'a jamais été aussi simple.
-            <br><br>
-            Des plans aériens stables et parfaitement exposés, avec un simple smartphone ?
-            Avec les drones Parrot, votre créativité suffit, prenez votre envol et saisissez les plus belles images, comme un pro.
-        </p>
-
-        <a href="#" class="section__btn btn__discover">Decouvrir</a>
+        <img src="wp-content/themes/wordpress-drones/_wordpress-drones/img/section3.png" alt="" class="thirdSection__img">
+        <div class="section__text">
+            <h2 class="section__title thirdSection__title">03</h2>
+            <h3 class="section__subTitle">Des technologies pour les pros</h3>
+            <p>Réussir des vidéos de grande qualité avec un drone n'a jamais été aussi simple.
+                <br><br>
+                Des plans aériens stables et parfaitement exposés, avec un simple smartphone ?
+                Avec les drones Parrot, votre créativité suffit, prenez votre envol et saisissez les plus belles images, comme un pro.
+            </p>
+            <a href="#" class="section__btn btn__discover">Decouvrir</a>
+        </div>
     </section>
     <div class="home__video">
         <video src="wp-content/themes/wordpress-drones/_wordpress-drones/video/promotion-vid.mp4" autoplay muted class="video"></video>
