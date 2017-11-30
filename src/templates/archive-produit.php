566ea35c0035e5f35ead8a889e1860293ecc4991
lucillef <lucille.franco@gmail.com.com>
2017-11-30 16:08:13 +0100

    debug html

--- src/templates/archive-produit.php
+++ src/templates/archive-produit.php
@@ -1,41 +1,42 @@
 <?php get_header() ?>
-    <?php
-		$the_query = new WP_Query( array( 'post_type' => 'produit' ) );
-    ?>
+<?php
+$the_query = new WP_Query( array( 'post_type' => 'produit' ) );
+?>
 <main id="main" class="site-main" role="main">
-  <div class="productsHeader">
-    <img class="products__img" src="<?php bloginfo('template_directory'); ?>/img/products_head.jpg" alt="">
-    <h1 class="products__title">Produits</h1>
-    <div class="filters">
-      <div class="category">
-        <input type="radio"  name="all"> Tout voir
-        <input type="radio"  name="drone"> Drones
-        <input type="radio"  name="mini-drone"> Minidrones
-      </div>
-      <span>Trier pars : </span>
-      <ul>
-        <li><a href="#">nouveauté</a></li>
-        <li><a href="#">prix croissant</a></li>
-        <li><a href="#">pri décroissant</a></li>
-      </ul>
+    <div class="productsHeader">
+        <img class="products__head" src="<?php bloginfo('template_directory'); ?>/img/products_head.jpg" alt="">
+        <h1 class="products__title">Produits</h1>
     </div>
-    <span>Catégorie :</span>
-  </div>
-  <?php if( $the_query->have_posts() ): ?>
-  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-      <section class="firstSection productsSection"><img class="products__img" src="" alt="">
-      <div class="products__left">
-        <h3 class="products__title"><?php echo get_the_title(); ?></h3>
-        <p class="products__description"><?php the_field('description'); ?>
-        </p>
-      </div>
-      <div class="products__right">
-        <span class="products__price"><?php the_field('price'); ?></span>
-        <a href="<?php the_permalink($post->ID); ?>" class="section__btn btn__discover">Acheter</a>
-      </div>
-    </section>
-  <?php endwhile; ?>
-  <?php endif; ?>
-  <?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+    <div class="filters">
+        <div class="category">
+            <span class="filter__title">Catégorie :</span>
+            <input type="radio"  name="all"> Tout voir
+            <input type="radio"  name="drone"> Drones
+            <input type="radio"  name="mini-drone"> Minidrones
+        </div>
+        <span class="filter__title">Trier pars : </span>
+        <ul>
+            <li><a href="#">nouveauté</a></li>
+            <li><a href="#">prix croissant</a></li>
+            <li><a href="#">pri décroissant</a></li>
+        </ul>
+    </div>
+    <?php if( $the_query->have_posts() ): ?>
+        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <section class="firstSection productsSection">
+                <img class="products__img" src="" alt="">
+                <div class="products__left">
+                    <h3 class="products__title section__subTitle"><?php echo get_the_title(); ?></h3>
+                    <p class="products__description"><?php the_field('description'); ?>
+                    </p>
+                </div>
+                <div class="products__right">
+                    <span class="products__price"><?php the_field('price'); ?>199€</span>
+                    <a href="<?php the_permalink($post->ID); ?>" class="section__btn btn__discover">Acheter</a>
+                </div>
+            </section>
+        <?php endwhile; ?>
+    <?php endif; ?>
+    <?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
 </main>
 <?php get_footer() ?>
