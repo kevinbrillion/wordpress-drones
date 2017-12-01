c1e47b27a0556164f1d9b67979bd67d348b5d7db
lucillef <lucille.franco@gmail.com.com>
2017-12-01 16:13:04 +0100

    style product

--- src/templates/single-produit.php
+++ src/templates/single-produit.php
@@ -4,42 +4,43 @@ global $wp;
 $url = home_url( $wp->request );
 $id = bwp_url_to_postid($url);
 $args = array(
-  'p'         => $id,
-  'post_type' => 'produit'
+    'p'         => $id,
+    'post_type' => 'produit'
 );
 $the_query = new WP_Query($args);
 ?>
 <?php if( $the_query->have_posts() ): ?>
     <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-      <?php $image_1 = get_field('image_1'); ?>
+        <?php $image_1 = get_field('image_1'); ?>
         <main id="main" class="site-main" role="main">
             <div class="productHeader">
             </div>
             <section class="productSection">
                 <div class="presentation__product">
+                    <?php if( !empty($image_1) ): ?><img class="products__img" src="<?php echo $image_1['url']; ?>" alt="<?php echo $image_1['alt']; ?>" /><?php endif; ?>
                     <div class="title">
                         <h3 class="products__title"><?php echo get_the_title(); ?></h3>
                         <p class="product__type"><?php  $terms = get_the_terms( get_the_ID(), 'category' );
-                                                        echo $terms[0]->name;
-                                                        ?></p>
+                            echo $terms[0]->name;
+                            ?></p>
                         <h4 class="product__descriptionTitle">Description</h4>
                         <p class="product__description"><?php the_field('description'); ?></p>
                     </div>
-                    <?php if( !empty($image_1) ): ?><img class="products__img" src="<?php echo $image_1['url']; ?>" alt="<?php echo $image_1['alt']; ?>" /><?php endif; ?>
                 </div>
                 <div class="product__right">
                     <span class="products__price"><?php the_field('prix'); ?></span>
-                    <a href="#" class="section__btn btn__discover">Acheter</a>
+                    <a href="#" class="btn__discover">Acheter</a>
                 </div>
             </section>
             <section class="techniqueSection">
-              <div class="technique__text"><?php the_field('technique_1'); ?></div>
-              <div class="technique__text"><?php the_field('technique_2'); ?></div>
-              <div class="technique__text"><?php the_field('technique_3'); ?></div>
-              <div class="technique__text"><?php the_field('technique_4'); ?></div>
+                <h4 class="product__descriptionTitle">Caractéristiques</h4>
+                <div class="technique__text"><?php the_field('technique_1'); ?></div>
+                <div class="technique__text"><?php the_field('technique_2'); ?></div>
+                <div class="technique__text"><?php the_field('technique_3'); ?></div>
+                <div class="technique__text"><?php the_field('technique_4'); ?></div>
             </section>
         </main>
     <?php endwhile; ?>
-  <?php endif; ?>
+<?php endif; ?>
 <?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
 <?php get_footer() ?>
